17807c2efd545580d65754d77e9324bb0b432e2d
Jannik Vogel <email@jannikvogel.de>
2016-03-13 04:02:46 +0100

    Spaces for indentation

--- attrib-align/source/main.c
+++ attrib-align/source/main.c
@@ -9,9 +9,9 @@
 #define CLEAR_COLOR 0x68B0D8FF
 
 #define DISPLAY_TRANSFER_FLAGS \
-	(GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | \
-	GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGB8) | \
-	GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO))
+  (GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | \
+  GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGB8) | \
+  GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO))
 
 
 static DVLB_s* vshader_dvlb;
@@ -24,52 +24,49 @@ size_t vbo_size = 1 * 1024; // 1 KiB should be plenty..
 
 static void sceneInit(void)
 {
-	// Load the vertex shader, create a shader program and bind it
-	vshader_dvlb = DVLB_ParseFile((u32*)vshader_shbin, vshader_shbin_size);
-	shaderProgramInit(&program);
-	shaderProgramSetVsh(&program, &vshader_dvlb->DVLE[0]);
-	C3D_BindProgram(&program);
+  // Load the vertex shader, create a shader program and bind it
+  vshader_dvlb = DVLB_ParseFile((u32*)vshader_shbin, vshader_shbin_size);
+  shaderProgramInit(&program);
+  shaderProgramSetVsh(&program, &vshader_dvlb->DVLE[0]);
+  C3D_BindProgram(&program);
 
-	// Get the location of the uniforms
-	uLoc_projection = shaderInstanceGetUniformLocation(program.vertexShader, "projection");
+  // Get the location of the uniforms
+  uLoc_projection = shaderInstanceGetUniformLocation(program.vertexShader, "projection");
 
-	// Compute the projection matrix
-	Mtx_OrthoTilt(&projection, 0.0, 400.0, 0.0, 240.0, 0.0, 1.0);
+  // Compute the projection matrix
+  Mtx_OrthoTilt(&projection, 0.0, 400.0, 0.0, 240.0, 0.0, 1.0);
 
-	// Create the VBO (vertex buffer object)
-	vbo_data = linearAlloc(vbo_size);
+  // Create the VBO (vertex buffer object)
+  vbo_data = linearAlloc(vbo_size);
 
-	// Configure the first fragment shading substage to just pass through the vertex color
-	// See https://www.opengl.org/sdk/docs/man2/xhtml/glTexEnv.xml for more insight
-	C3D_TexEnv* env = C3D_GetTexEnv(0);
-	C3D_TexEnvSrc(env, C3D_Both, GPU_PRIMARY_COLOR, 0, 0);
-	C3D_TexEnvOp(env, C3D_Both, 0, 0, 0);
-	C3D_TexEnvFunc(env, C3D_Both, GPU_REPLACE);
+  // Configure the first fragment shading substage to just pass through the vertex color
+  // See https://www.opengl.org/sdk/docs/man2/xhtml/glTexEnv.xml for more insight
+  C3D_TexEnv* env = C3D_GetTexEnv(0);
+  C3D_TexEnvSrc(env, C3D_Both, GPU_PRIMARY_COLOR, 0, 0);
+  C3D_TexEnvOp(env, C3D_Both, 0, 0, 0);
+  C3D_TexEnvFunc(env, C3D_Both, GPU_REPLACE);
 }
 
 
 void draw(size_t stride, unsigned int attrib_count, unsigned int permutation, int pad_type, int pad_count, int type) {
 
-    // Configure buffers
-    C3D_BufInfo* bufInfo = C3D_GetBufInfo();
-    BufInfo_Init(bufInfo);
-    BufInfo_Add(bufInfo, vbo_data, stride, attrib_count, permutation);
+  // Configure buffers
+  C3D_BufInfo* bufInfo = C3D_GetBufInfo();
+  BufInfo_Init(bufInfo);
+  BufInfo_Add(bufInfo, vbo_data, stride, attrib_count, permutation);
 
-	  // Configure attributes for use with the vertex shader
-	  C3D_AttrInfo* attrInfo = C3D_GetAttrInfo();
-	  AttrInfo_Init(attrInfo);
-    AttrInfo_AddLoader(attrInfo, 0, GPU_FLOAT, 2); // v0=position
-    AttrInfo_AddLoader(attrInfo, 1, pad_type, pad_count); // v1=pad
-    AttrInfo_AddLoader(attrInfo, 2, type, 3); // v2=color
-//	  AttrInfo_AddFixed(attrInfo, 1); // v1=color
-	// Set the fixed attribute (color) to solid white
-//	C3D_FixedAttribSet(1, 1.0, 1.0, 1.0, 1.0);
+  // Configure attributes for use with the vertex shader
+  C3D_AttrInfo* attrInfo = C3D_GetAttrInfo();
+  AttrInfo_Init(attrInfo);
+  AttrInfo_AddLoader(attrInfo, 0, GPU_FLOAT, 2); // v0=position
+  AttrInfo_AddLoader(attrInfo, 1, pad_type, pad_count); // v1=pad
+  AttrInfo_AddLoader(attrInfo, 2, type, 3); // v2=color
 
-	// Update the uniforms
-	C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_projection, &projection);
+  // Update the uniforms
+  C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_projection, &projection);
 
-	// Draw the VBO
-	C3D_DrawArrays(GPU_TRIANGLES, 0, 3);
+  // Draw the VBO
+  C3D_DrawArrays(GPU_TRIANGLES, 0, 3);
 }
 
 static int sceneRender(unsigned int mode)
@@ -171,60 +168,60 @@ static int sceneRender(unsigned int mode)
 
 static void sceneExit(void)
 {
-	// Free the VBO
-	linearFree(vbo_data);
+  // Free the VBO
+  linearFree(vbo_data);
 
-	// Free the shader program
-	shaderProgramFree(&program);
-	DVLB_Free(vshader_dvlb);
+  // Free the shader program
+  shaderProgramFree(&program);
+  DVLB_Free(vshader_dvlb);
 }
 
 int main()
 {
-	// Initialize graphics
-	gfxInitDefault();
+  // Initialize graphics
+  gfxInitDefault();
 
   consoleInit(GFX_BOTTOM, NULL);
 
-	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+  C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 
-	// Initialize the render target
-	C3D_RenderTarget* target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
-	C3D_RenderTargetSetClear(target, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
-	C3D_RenderTargetSetOutput(target, GFX_TOP, GFX_LEFT, DISPLAY_TRANSFER_FLAGS);
+  // Initialize the render target
+  C3D_RenderTarget* target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+  C3D_RenderTargetSetClear(target, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
+  C3D_RenderTargetSetOutput(target, GFX_TOP, GFX_LEFT, DISPLAY_TRANSFER_FLAGS);
 
-	// Initialize the scene
-	sceneInit();
+  // Initialize the scene
+  sceneInit();
 
-	// Main loop
-	while (aptMainLoop())
-	{
-		hidScanInput();
+  // Main loop
+  while (aptMainLoop())
+  {
+    hidScanInput();
 
-		// Respond to user input
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_SELECT) break;
+    // Respond to user input
+    u32 kDown = hidKeysDown();
+    if (kDown & KEY_SELECT) break;
     bool is_pressed = kDown & KEY_START;
     static bool was_pressed = true;
     static unsigned int mode = 0;
-		if (was_pressed != is_pressed && is_pressed) {
+    if (was_pressed != is_pressed && is_pressed) {
       mode++;
       printf("Switched to mode %d!\n", mode);
     }
     was_pressed = is_pressed;
 
-		// Render the scene
-		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-			C3D_FrameDrawOn(target);
-			mode = sceneRender(mode);
-		C3D_FrameEnd(0);
-	}
+    // Render the scene
+    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+      C3D_FrameDrawOn(target);
+      mode = sceneRender(mode);
+    C3D_FrameEnd(0);
+  }
 
-	// Deinitialize the scene
-	sceneExit();
+  // Deinitialize the scene
+  sceneExit();
 
-	// Deinitialize graphics
-	C3D_Fini();
-	gfxExit();
-	return 0;
+  // Deinitialize graphics
+  C3D_Fini();
+  gfxExit();
+  return 0;
 }
