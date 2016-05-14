87d472f7087f169f1362d961991749deb868e77f
Jannik Vogel <email@jannikvogel.de>
2016-05-14 14:42:08 +0200

    Fix fpscr code

--- fpscr/source/main.c
+++ fpscr/source/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#include "thread.h"
 
 #include <3ds.h>
 
@@ -13,15 +17,33 @@ uint32_t get_fpscr(void) {
 void thread_func(void* arg) {
   uint32_t old_fpscr = *(uint32_t*)arg;
   uint32_t new_fpscr = get_fpscr();
-  printf("old_fpscr: 0x%08" PRIX32 "\n", old_fpscr);
-  printf("new_fpscr: 0x%08" PRIX32 "\n", new_fpscr);
-  svcExitProcess();
+  FILE* f = fopen("out-fpscr.txt", "w");
+  fprintf(f, "old_fpscr: 0x%08" PRIX32 "\n", old_fpscr);
+  fprintf(f, "new_fpscr: 0x%08" PRIX32 "\n", new_fpscr);
+  fclose(f);
+  svcExitThread();
 }
 
 int main() {
   static uint32_t old_fpscr;
   old_fpscr = get_fpscr();
+
+  size_t stack_size = 0x10000;
+#if 1
+  Thread thread = threadCreate(thread_func, &old_fpscr, stack_size, 0x3F, -2, false);
+  threadJoin(thread, U64_MAX);
+  threadFree(thread);
+#else
+  // Didn't work in citra for me..?
   Handle thread;
-  Result res = svcCreateThread(&thread, thread_func, &old_fpscr, ..., 0x20, 0);
-  while(1);
+  uint8_t* stack = memalign(32, stack_size);
+  Result res = svcCreateThread(&thread, thread_func, (u32)&old_fpscr, (u32*)&stack[stack_size - 8], 0x3F, -2);
+	if (R_FAILED(res)) {
+    fprintf(f, "Failed to create thread\n");
+    return 1;
+  }
+  svcWaitSynchronization(thread, U64_MAX);
+#endif
+
+  return 0;
 }
